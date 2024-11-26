205a2a41e13dbe924e9168b62ec8dd1549695c52
Anna Mercado <nissathar.15@gmail.com>
2024-11-26 18:10:22 +0100

    ejercicio 6 tema 2 acabado

---
+++ Tema_2/Nivell_1/Exercici_6.php
@@ -0,0 +1,18 @@
+<?php
+
+function isBitten(){
+    $bitten = false;
+    $random = rand(1,0);
+    if ($random == 1){
+        $bitten = true;
+    }
+
+    return $bitten;
+}
+if (isBitten()) {
+    echo "¡Charlie te ha mordido!";
+} else {
+    echo "Esta vez te libraste de ser mordido.";
+}
+
+?>
