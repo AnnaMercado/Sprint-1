c3991aecefe334a53cf8147bd94ce082f291a1be
Anna Mercado <nissathar.15@gmail.com>
2024-11-26 13:57:04 +0100

    Exercicio 1 y 2 del Tema 2 y avances en el Tema 1 nivel 1 (sin acabar)

---
+++ Tema_2/Nivell_1/Exercici_1.php
@@ -0,0 +1,10 @@
+<?php
+
+    $es_Boleano = true;
+    $es_Integer = 4;
+    $es_Double = 4.5;
+    $es_String = "Hello, World!"; 
+    define('MI_NOMBRE', "Anna Mercado Pozo"); 
+    echo "<h1>".MI_NOMBRE."</h1>";
+
+?>
---
+++ Tema_2/Nivell_1/Exercici_2.php
@@ -0,0 +1,24 @@
+<?php
+$string = "Hellow World!";
+$string2 = "Aquest és el curs de PHP.";
+
+echo $string . "\n";
+
+$string_modificada = strtoupper($string);
+echo $string_modificada. "\n";
+
+$string_modificada = strrev($string);
+echo $string_modificada . "\n";
+
+echo strlen($string). "\n"; 
+
+echo $string . " " . $string2; 
+
+
+
+
+
+
+
+
+?>
