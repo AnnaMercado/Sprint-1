612b0ffdd7ad7f938c9283330451aac5638d9389
Anna Mercado <nissathar.15@gmail.com>
2025-05-07 17:49:36 +0200

    tema 3 nivel 2 ejercicio 1

--- Tema_3/Nivell_2/Exercici_1.php
+++ Tema_3/Nivell_2/Exercici_1.php
@@ -1,11 +1,14 @@
 <?php
+    $array1 = [1, 3.5, 7, 2, 5.0, 10];
+    $array2 = [3.5, 2, 8, 10, 12, 5];
 
-Crea un programa que contingui dos arrays de nombres enters/floats. Un cop creats, mostra per pantalla el resultat de:
+    $intersection = array_intersect($array1, $array2);
 
-    La intersecció dels dos arrays (nombres en comú)
-    La mescla dels dos arrays
+    $merged = array_merge($array1, $array2);
 
-    
+    echo "Intersection of the arrays:\n";
+    print_r($intersection);
 
-
+    echo "Merged arrays:\n";
+    print_r($merged);
 ?>
